862cfad1c1a20b4fd4036ace9f573e21830fa718
hqattous <hqattous@yahoo.com>
2021-05-20 10:53:06 +0300

    Startup changed

--- MyReopTestApp/MyReopTestApp/Startup.cs
+++ MyReopTestApp/MyReopTestApp/Startup.cs
@@ -32,7 +32,7 @@ namespace MyReopTestApp
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hazem Qattous");
                 });
             });
         }
